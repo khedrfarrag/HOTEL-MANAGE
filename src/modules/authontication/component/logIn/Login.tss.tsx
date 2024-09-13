357a236275dcab873b5ecf4444be138dc4110bad
khedr farrag <khedr.farrag@gmail.com>
2024-09-13 11:47:43 +0300

    wlll

--- src/modules/authontication/component/logIn/Login.tss.tsx
+++ src/modules/authontication/component/logIn/Login.tss.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { HotelsList } from "../../../ParrelFiles";
-import { signin } from "../../../../assets/ParrelAssets/Parrel";
+import React from 'react';
+import { HotelsList } from '../../../ParrelFiles';
+import { signin } from '../../../../assets/ParrelAssets/Parrel';
 
 export default function Login() {
   return (
     <>
-      <div>Loginssss</div>
-      {/* <img src={signin} alt="" /> */}
-      <HotelsList />
+      <div>login</div>
     </>
   );
 }
