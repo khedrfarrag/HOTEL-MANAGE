afbde65ae1f569648f0afe09a4f9cdded18475bf
Amr <eng.amrabdelwahab02@gmail.com>
2024-09-13 16:36:11 +0300

    thirdCommit

--- src/modules/shared/component/masterLayout/MasterLayOut.tsx
+++ src/modules/shared/component/masterLayout/MasterLayOut.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import NavBar from "../navBar/NavBar";
-import SideBar from "../sideBar/SideBar";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from '../navBar/NavBar';
+import SideBar from '../sideBar/SideBar';
 
 export default function MasterLayOut() {
   return (
     <>
-      <div>
-        <div></div>
+      <div className="d-flex">
         <div>
           <SideBar />
+        </div>
+        <div>
           <NavBar />
           <Outlet />
         </div>
