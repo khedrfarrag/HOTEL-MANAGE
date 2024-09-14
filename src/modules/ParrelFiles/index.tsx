56da87720d4a30c2d932b3b70ff4a54997d6e039
khedr farrag <khedr.farrag@gmail.com>
2024-09-14 03:15:39 +0300

    upWork

--- src/modules/ParrelFiles/index.tsx
+++ src/modules/ParrelFiles/index.tsx
@@ -6,6 +6,11 @@ import ResetPass from '../authontication/component/resetPass/ResetPass';
 import VerifyAccount from '../authontication/component/verifyAccount/VerifyAccount';
 import NavBar from '../shared/component/navBar/NavBar';
 import SideBar from '../shared/component/sideBar/SideBar';
+import AdminMasterLayout from '../shared/component/adminMasterLayout/AdminMasterLayout';
+import NotFound from '../shared/component/notFound/NotFound';
+import ProtectedRoute from '../shared/component/protectedRoute/ProtectedRoute';
+import UserMasterLayout from '../shared/component/userMasterLayout/UserMasterLayout';
+import AuthLayout from '../shared/component/authLayout/AuthLayout';
 import LandingPage from '../uers/components/LandingPage/LandingPage';
 import Booking from '../uers/components/booking/Booking';
 import ChangePass from '../uers/components/changePass/ChangePass';
@@ -13,7 +18,21 @@ import DetailsPage from '../uers/components/detailsPage/DetailsPage';
 import Explore from '../uers/components/explore/Explore';
 import Favorites from '../uers/components/favorites/Favorites';
 import usersDataContext from '../uers/context/usersDataContext';
+import Ads from '../admin/components/ads/Ads';
+import BookingList from '../admin/components/bookingList/BookingList';
+import FacilitiesList from '../admin/components/facilitiesList/FacilitiesList';
+import Home from '../admin/components/home/Home';
+import RoomsList from '../admin/components/roomList/RoomsList';
+import AddOrEditRooms from '../admin/components/rooms/AddOrEditRooms';
+import UsersList from '../admin/components/usersList/UsersList';
+import adminDataContext from '../admin/context/adminDataContext';
+
 export {
+  ProtectedRoute,
+  AdminMasterLayout,
+  UserMasterLayout,
+  AuthLayout,
+  NotFound,
   SideBar,
   NavBar,
   Login,
@@ -28,4 +47,12 @@ export {
   Explore,
   Favorites,
   usersDataContext,
+  Ads,
+  BookingList,
+  FacilitiesList,
+  Home,
+  RoomsList,
+  AddOrEditRooms,
+  UsersList,
+  adminDataContext,
 };
---
+++ src/modules/admin/context/adminDataContext.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function adminDataContext() {
+  return <div>adminDataContext</div>;
+}
